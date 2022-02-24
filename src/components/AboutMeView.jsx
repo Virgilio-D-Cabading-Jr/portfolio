16090acbfeb3e12174d72ee00e6b823e22b64c4d
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-02-23 19:56:35 -0800

    remover import topic button comp in about me view

--- src/components/AboutMeView.jsx
+++ src/components/AboutMeView.jsx
@@ -1,8 +1,6 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
 
-import TopicButtonComp from './TopicButtonComp';
-
 ////////////////////////////////////////////////////
 //  ABOUT ME VIEW
 ////////////////////////////////////////////////////
