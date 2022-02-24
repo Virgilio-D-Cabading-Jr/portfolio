1c7800c249b9a3543d1c9ec1d124725b5a85620e
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-02-23 19:49:15 -0800

    correct wording in Home View

--- src/components/HomeView.jsx
+++ src/components/HomeView.jsx
@@ -13,12 +13,12 @@ const HomeView = () => {
         <div className=''>
           <h1 className='main-name text-yellow mt-3'><strong>Virgilio D. Cabading Jr.</strong></h1>
           <h1>Software Developer</h1>
-          <p>Hello, I am a highly motivated Software Developertransitioning from the healthcare industry<br/>with a B.S. in Informatics.My interests are many, but I am currently interested in<br/>expanding my knowledge on Javascript (React), Java (Spring) and Python (Flask).</p>
+          <p>Hello, I am a highly motivated Software Developer transitioning from the healthcare industry<br/>with a B.S. in Informatics.  My interests are many, but I am currently interested in<br/>expanding my knowledge on building a React.js front-end with Express(MongoDB) or Java(MySQL) backend along with learning DevOps for AWS and Azure</p>
         </div>
         <div className='btn-row row'>
-          <Link to="/aboutme" className='home-btn col btn m-3' ><h3><strong>About Me 🔎</strong></h3></Link>
-          <Link to="/projects" className='home-btn col btn m-3' ><h3><strong>Projects 🍎</strong></h3></Link>
-          <Link to="/contactinfo" className='home-btn col btn m-3' ><h3><strong>Contact info 📞</strong></h3></Link>
+          <Link to="/aboutme" className='home-btn col btn m-3' ><h3>About Me</h3></Link>
+          <Link to="/projects" className='home-btn col btn m-3' ><h3>Projects</h3></Link>
+          <Link to="/contactinfo" className='home-btn col btn m-3' ><h3>Contact info</h3></Link>
         </div>
       </div>
     </div>
