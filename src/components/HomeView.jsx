90abeead8ed79753bf147eb7880d96cd02255d48
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-02-23 20:22:10 -0800

    update links in homeview

--- src/components/HomeView.jsx
+++ src/components/HomeView.jsx
@@ -16,9 +16,9 @@ const HomeView = () => {
           {/* <p>Hello, I am a highly motivated Software Developer transitioning from the healthcare industry<br/>with a B.S. in Informatics.  My interests are many, but I am currently interested in<br/>expanding my knowledge on building a React.js front-end with various backend frameworks along with DevOps.</p> */}
         </div>
         <div className='btn-row row'>
-          <Link to="/aboutme" className='home-btn col btn m-3' ><h3>About Me</h3></Link>
-          <Link to="/projects" className='home-btn col btn m-3' ><h3>Projects</h3></Link>
-          <Link to="/contactinfo" className='home-btn col btn m-3' ><h3>Contact info</h3></Link>
+          <Link to="/portfolio/aboutme" className='home-btn col btn m-3' ><h3>About Me</h3></Link>
+          <Link to="/portfolio/projects" className='home-btn col btn m-3' ><h3>Projects</h3></Link>
+          <Link to="/portfolio/contactinfo" className='home-btn col btn m-3' ><h3>Contact info</h3></Link>
         </div>
       </div>
     </div>
