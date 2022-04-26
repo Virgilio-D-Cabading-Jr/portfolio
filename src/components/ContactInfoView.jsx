439220a9a295c23deba94420bf27cff6379dc435
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-04-26 05:22:20 -0700

    update email and resume format in contact info

--- src/components/ContactInfoView.jsx
+++ src/components/ContactInfoView.jsx
@@ -10,23 +10,19 @@ const ContactInfoView = () => {
       <h1>Contact Information</h1>
       {/* Email */}
       <div className='row m-3'>
-        <div className='col-3 d-flex justify-content-end'>
-          <img className='img-thumb' src="https://i.ibb.co/3M8NM8j/email-thumb.png" alt="email" />
-        </div>
+        <img className='col-1' src="https://i.ibb.co/3M8NM8j/email-thumb.png" alt="email" />
         <div className='col-9'>
           <h3>virgilio.d.cabading.jr@gmail.com</h3>
         </div>
       </div>
       {/* Resume */}
       <div className='row m-3'>
-        <div className='col-3 d-flex justify-content-end'>
-          <img className='img-thumb' src="https://i.ibb.co/q7rkpRS/resume-thumb.png" alt="resume" />
-        </div>
+        <img className='col-1' src="https://i.ibb.co/q7rkpRS/resume-thumb.png" alt="resume" />
         <div className='col-9'>
           <a href="https://drive.google.com/file/d/1zNYGycxB7ivAclbv6XxiJojFpPPK-h0B/view?usp=sharing"
               target="_blank"
               rel="noopener noreferrer">
-            <h3>Resume</h3>
+            <h3 className='text-light'>Resume</h3>
           </a>
         </div>
       </div>
